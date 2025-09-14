096a943ff008b4ac347dbe0ec0f1bca46a72d500
makatech2147 <makatech2147@gmail.com>
2025-09-14 14:00:51 +0100

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,17 +1,1 @@
-// app/layout.tsx
-export const metadata = {
-  title: 'StyleForge Platform',
-  description: 'A platform for fashion industry stakeholders',
-}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
-}
