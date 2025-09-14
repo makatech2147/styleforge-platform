03ca9603f69546b10384cc4e976fd7e143a7a52d
makatech2147 <makatech2147@gmail.com>
2025-09-14 15:30:22 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // No complex settings for now
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
 }
 
 module.exports = nextConfig
