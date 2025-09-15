5a56fafec5cf318d99ff1a757e3ca934cec9ab7a
makatech2147 <makatech2147@gmail.com>
2025-09-15 21:12:19 +0100

    Create signin.js

---
+++ pages/auth/signin.js
@@ -0,0 +1,25 @@
+import { getCsrfToken } from "next-auth/react"
+
+export default function SignIn({ csrfToken }) {
+  return (
+    <form method="post" action="/api/auth/callback/credentials">
+      <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+      <label>
+        Email
+        <input name="email" type="email" />
+      </label>
+      <label>
+        Password
+        <input name="password" type="password" />
+      </label>
+      <button type="submit">Sign in</button>
+    </form>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const csrfToken = await getCsrfToken(context)
+  return {
+    props: { csrfToken },
+  }
+}
